b1b942a070a6fa9633f7fc8bc07c99c408c4b204
unknown <nferrandez-tartarin@gamingcampus.local>
2024-02-07 12:16:17 +0100

    Selection personnage

--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ class Program
     static void Main(string[] args)
     {
 
-        string playerAffichage = "Statistiques/Player_Affichage.csv";
-        FileManager.LecteurFichier(playerAffichage);
+        /*string playerAffichage = "Statistiques/Player_Affichage.csv";
+        FileManager.LecteurFichier(playerAffichage);*/
 
-        Console.Write("Bonjour tout le monde \n");
+        /*Console.Write("Bonjour tout le monde \n");
         Console.Write("Vous commencez l aventure Savun low cost! \n");
 
         char[,] donjon = Map.GenererDonjon();
         int[,] map1 = Map.GenererMap1();
-        Input.MovePlayer(map1);
+        Input.MovePlayer(map1);*/
+
+        Player.Stockeur();
 
         Console.ReadKey();
     }
